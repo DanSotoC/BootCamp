d10c6888751d15d2a6dd5039fe68438d9c4cdbf9
DanSotoC <danielsotocorrea21@hotmail.com>
2022-04-04 23:23:05 -0400

    Subida Proyecto

---
+++ CampBoot/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Main {
+	public static void main(String[] args) {
+		SumaEntreSi(5,7,9);
+	}
+	
+	public static void SumaEntreSi(int a, int b, int c) {
+		int suma = a+ b+ c;
+		System.out.print("La Suma de los numeros es: "+ suma);
+	}
+}
