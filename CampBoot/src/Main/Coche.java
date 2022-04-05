bbef9a16333df995922fe068dd0e09f454abaf4b
DanSotoC <danielsotocorrea21@hotmail.com>
2022-04-05 00:11:31 -0400

    Tarea 1

--- CampBoot/src/Main/Coche.java
+++ CampBoot/src/Main/Coche.java
@@ -1,7 +1,23 @@
 package Main;
 
 public class Coche {
-	int cantPuertas;
 	
+	int cantPuertas;
+
+	public Coche(int cantPuertas) {
+		super();
+		this.cantPuertas = cantPuertas;
+	}
+
+	public void AumentoNumeroPuertas(){
+		this.cantPuertas++;
+		System.out.print("El coche tiene "+cantPuertas+" puertas");
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 	
 }
--- CampBoot/src/Main/Main.java
+++ CampBoot/src/Main/Main.java
@@ -1,12 +1,20 @@
 package Main;
 
 public class Main {
-	public static void main(String[] args) {
-		SumaEntreSi(5,7,9);
-	}
+	
 	
 	public static void SumaEntreSi(int a, int b, int c) {
 		int suma = a+ b+ c;
-		System.out.print("La Suma de los numeros es: "+ suma);
+		System.out.println("La Suma de los numeros es: "+ suma);
 	}
+	
+	static Coche miCoche = new Coche(4);
+	
+	public static void main(String[] args) {
+		SumaEntreSi(5,7,9);
+		//miCoche.AumentoNumeroPuertas();
+		System.out.println("La Cantidad de puertas de mi Coche son: "+ miCoche.cantPuertas);
+	}
+
+
 }
